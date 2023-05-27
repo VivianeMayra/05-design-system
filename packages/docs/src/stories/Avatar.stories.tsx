cf8bfd4ecf2fb48e821ca9acfbb2a4330f6dc763
Viviane Mayra <vivi.mayra01@gmail.com>
2023-05-27 15:34:40 -0300

    Publicando Storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: "https://github.com/VivianeMayra.png",
     alt: "Viviane Mayra",
   },
+  argTypes: {
+    src: {
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
